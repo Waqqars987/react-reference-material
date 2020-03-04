5a0bdd818ceba239f5ee18a89a400040b053c19e
Waqqars987 <waqqars987@gmail.com>
2020-03-04 19:16:10 +0530

    debugging completed

--- react-complete-guide/3. styling-react/src/App.js
+++ react-complete-guide/3. styling-react/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 // import Radium, { StyleRoot } from 'radium';
 // import styled from 'styled-components';
 
@@ -78,14 +77,13 @@ class App extends Component {
 				<div>
 					{this.state.persons.map((person, index) => {
 						return (
-							<ErrorBoundary key={person.id}>
-								<Person
-									click={() => this.deletePersonHandler(index)}
-									name={person.name}
-									age={person.age}
-									changed={(event) => this.nameChangedHandler(event, person.id)}
-								/>
-							</ErrorBoundary>
+							<Person
+								click={() => this.deletePersonHandler(index)}
+								name={person.name}
+								age={person.age}
+								changed={(event) => this.nameChangedHandler(event, person.id)}
+								key={person.id}
+							/>
 						);
 					})}
 				</div>
