8bacca879e5db2adb6d9f8194276933674186af1
Waqqars987 <Waqqars987@gmail.com>
2021-04-11 00:22:44 +0530

    completed section 6

---
+++ nextjs-and-react-the-complete-guide/6. project-time-page-prerendering-and-data-fetching/pages/index.js
@@ -0,0 +1,23 @@
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+	return (
+		<div>
+			<EventList items={props.featuredEvents} />
+		</div>
+	);
+}
+
+export async function getStaticProps() {
+	const featuredEvents = await getFeaturedEvents();
+
+	return {
+		props: {
+			featuredEvents,
+		},
+		revalidate: 1800,
+	};
+}
+
+export default HomePage;
