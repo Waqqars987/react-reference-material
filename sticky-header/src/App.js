d0de2798c4a8d8757cc93923ad6bee6c4b3d5570
Waqqars987 <waqqars987@gmail.com>
2021-08-20 22:26:02 +0530

    refactoring and browser compatibility improvements

--- sticky-header/src/App.js
+++ sticky-header/src/App.js
@@ -1,12 +1,18 @@
+// import { useRef } from 'react';
+
 import useSticky from './useSticky';
 import './App.css';
 
 const App = () => {
-	const { isSticky, stickyElementRef, stickyContextRef } = useSticky();
+	// const myStickyElementRef = useRef(null);
+	// const myStickyContextRef = useRef(null);
+	// const { isSticky } = useSticky(myStickyElementRef, myStickyContextRef);
+
+	const { isSticky, stickstickyElementRef, stickyContextRef } = useSticky();
 
 	return (
 		<>
-			<header className={isSticky ? 'navbar sticky' : 'navbar'} ref={stickyElementRef}>
+			<header className={isSticky ? 'navbar sticky' : 'navbar'} ref={stickstickyElementRef}>
 				<nav className='navigation'>{isSticky ? 'STICKY' : ''} NAV</nav>
 			</header>
 
--- sticky-header/src/useSticky.js
+++ sticky-header/src/useSticky.js
@@ -2,39 +2,55 @@ import { useState, useEffect, useCallback, useRef } from 'react';
 
 /**
  *
+ * @param {*} customStickyElementRef [optional]: sticky element reference to be passed by the User
+ * @param {*} customStickyContextRef [optional]: sticky context reference to be passed by the User
  * @returns {
- * isSticky: whether the concerned sticky element is in sticky state
- * stickyElementRef: need to be referred to the element that has to be made sticky
- * stickyContextRef: need to be referred to the sticky positioning context element (usage is optional)
+ * isSticky: whether the concerned sticky element is in sticky state,
+ * stickyElementRef [optional]: need to be referred to the element that has to be made sticky [returned only when customStickyElementRef is not passed by the User],
+ * stickyContextRef [optional]: need to be referred to the sticky positioning context element [returned only when customStickyContextRef is not passed by the User,utilization is optional]
  * }
  */
-const useSticky = () => {
+const useSticky = (customStickyElementRef, customStickyContextRef) => {
 	const [isSticky, setIsSticky] = useState(false);
-	const stickyElementRef = useRef(null);
-	const stickyContextRef = useRef(null);
+
+	const implicitStickyElementRef = useRef(null);
+	const implicitStickyContextRef = useRef(null);
+
+	const stickyElementRef = customStickyElementRef || implicitStickyElementRef;
+	const stickyContextRef = customStickyContextRef || implicitStickyContextRef;
 
 	const handleScroll = useCallback(() => {
-		const currentVerticalOffset = window.scrollY;
-		const verticalOffsetLimit = stickyElementRef?.current?.getBoundingClientRect().bottom || 50; //default vertical offset limit is 50px
+		const currentVerticalOffset = window.scrollY || window.pageYOffset;
+
+		//default vertical offset limit is 50px
+		const verticalOffsetLimit = stickyElementRef.current
+			? stickyElementRef.current.getBoundingClientRect().bottom
+			: 50;
 
 		const shouldStick =
 			(currentVerticalOffset > verticalOffsetLimit && !stickyContextRef.current) ||
 			(currentVerticalOffset > verticalOffsetLimit &&
-				currentVerticalOffset <= stickyContextRef.current.scrollHeight);
+				currentVerticalOffset <=
+					(stickyContextRef.current && stickyContextRef.current.scrollHeight));
 
 		if (shouldStick) {
 			!isSticky && setIsSticky(true);
 		} else {
 			isSticky && setIsSticky(false);
 		}
-	}, [isSticky, stickyElementRef]);
+	}, [isSticky, stickyElementRef, stickyContextRef]);
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
+
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, [handleScroll]);
 
-	return { isSticky, stickyElementRef, stickyContextRef };
+	return {
+		isSticky,
+		...(!customStickyElementRef && { stickyElementRef }),
+		...(!customStickyContextRef && { stickyContextRef }),
+	};
 };
 
 export default useSticky;
