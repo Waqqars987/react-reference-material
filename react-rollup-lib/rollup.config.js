2547a945726ff2d2e623ac266ea698acae43c19e
Waqqar Suleman <waqqars987@gmail.com>
2022-06-14 23:11:13 +0530

    updated rollup config

--- react-rollup-lib/rollup.config.js
+++ react-rollup-lib/rollup.config.js
@@ -1,38 +1,46 @@
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
 import { sync } from 'glob';
+
+const isDevelopmentMode = process.env.ROLLUP_WATCH;
 
 export default {
 	input: sync('src/**/index.js'),
 	output: {
 		dir: 'dist',
 		format: 'es',
-		sourcemap: true,
+		sourcemap: 'inline',
 		compact: true,
 		preserveModules: true,
-		preserveModulesRoot: 'src',
+		preserveModulesRoot: 'src'
 	},
 	watch: {
-		exclude: 'node_modules/**',
+		exclude: 'node_modules/**'
 	},
 	plugins: [
 		peerDepsExternal(),
-		image(),
+		image({ dom: true }),
 		postcss({
 			minimize: true,
+			sourceMap: 'inline',
+			extract: 'styles.min.css'
 		}),
 		nodeResolve({
 			extensions: ['.js', '.jsx'],
 			moduleDirectories: ['src', 'node_modules'],
+			browser: true,
+			preferBuiltins: false
 		}),
 		babel({
 			babelHelpers: 'runtime',
 			exclude: 'node_modules/**',
 			plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
-			presets: ['@babel/preset-react'],
+			presets: ['@babel/preset-env', '@babel/preset-react']
 		}),
-	],
+		isDevelopmentMode ? false : terser()
+	]
 };
