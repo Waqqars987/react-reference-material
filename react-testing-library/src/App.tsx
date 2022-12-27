1e17fa5d72e9fea34dd1458996ccc0795adfdca9
Waqqar Suleman <waqqars987@gmail.com>
2022-12-27 15:52:24 +0530

    completed user interaction section

--- react-testing-library/src/App.tsx
+++ react-testing-library/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
+import { Counter } from './components/counter/Counter';
 // import { Application } from './components/application/Application';
-import { Skills } from './components/skills/Skills';
+// import { Skills } from './components/skills/Skills';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* <Application /> */}
-			<Skills skills={['HTML', 'CSS', 'JavScript']} />
+			{/* <Skills skills={['HTML', 'CSS', 'JavScript']} /> */}
+			<Counter />
 		</div>
 	);
 }
