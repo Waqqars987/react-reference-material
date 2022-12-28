abe2b52df71971284bbbf6e3fc86bfc81801a85a
Waqqar Suleman <waqqars987@gmail.com>
2022-12-28 12:43:47 +0530

    completed Providers section

--- react-testing-library/src/App.tsx
+++ react-testing-library/src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css';
-import { Counter } from './components/counter/Counter';
 // import { Application } from './components/application/Application';
+// import { CounterTwo } from './components/counter-two/CounterTwo';
+// import { Counter } from './components/counter/Counter';
+import { MuiMode } from './components/mui/MuiMode';
 // import { Skills } from './components/skills/Skills';
+// import { Users } from './components/users/Users';
+import { AppProviders } from './providers/AppProviders';
 
 function App() {
 	return (
-		<div className='App'>
-			{/* <Application /> */}
-			{/* <Skills skills={['HTML', 'CSS', 'JavScript']} /> */}
-			<Counter />
-		</div>
+		<AppProviders>
+			<div className='App'>
+				{/* <Application />
+				<Skills skills={['HTML', 'CSS']} />
+				<Counter /> */}
+				{/* <CounterTwo count={1} />
+				<Users /> */}
+				<MuiMode />
+			</div>
+		</AppProviders>
 	);
 }
 
